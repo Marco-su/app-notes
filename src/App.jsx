9aa227de4e228d98fa47bdafbcd178cdcab617ef
marco <marco_subero1996@hotmail.com>
2021-03-07 01:13:30 -0400

    Barra de navegacion agregada

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./common/Navbar";
+import Notes from "./components/Notes";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Notes />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/common/Navbar.jsx
@@ -0,0 +1,14 @@
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div id="navbar">
+      <div>
+        <span>Notes App</span>
+        <button className="btn btn-success">Crear Nota</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
